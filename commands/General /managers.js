871697651beeadb9a849f512793048238099f75e
Yoosuf Mohamed <yoosufmohamed@Yoosufs-MBP.lan>
2022-10-26 17:42:10 -0700

    Bug Fix

--- commands/General /managers.js
+++ commands/General /managers.js
@@ -9,31 +9,38 @@ module.exports = {
         let managers = new DotJson('managers.json');
 
         const ROLES = managers.get("ROLES");
-        const USERS = managers.get("USERS")
+        const USERS = managers.get("USERS");
+
         const GUILD = client.guilds.cache.find(_guild => _guild.id === GUILD_ID);
 
-        let rolesStr = ""
-        let usersStr = ""
+        let rolesStr;
+        let usersStr;
 
-        USERS.forEach((user) => {
-            const member = client.users.cache.find(_user => _user.id === user)
-            if(!member) return
-            usersStr += `\`${member.tag}\` \n`
-        })
+        if(USERS.length > 0){
+            usersStr = "";
+            USERS.forEach((user) => {
+                const member = client.users.cache.find(_user => _user.id === user)
+                if(!member) return
+                usersStr += `\`${member.tag}\` \n`
+            })
+        }
 
-        ROLES.forEach((role) => {
-            const guildRole = GUILD.roles.cache.find(_role => _role.id === role)
-            if(!guildRole) return
-            rolesStr += `\`${guildRole.name}\` \n`
-        })
+        if(ROLES.length > 0){
+            rolesStr = "";
+            ROLES.forEach((role) => {
+                const guildRole = GUILD.roles.cache.find(_role => _role.id === role)
+                if(!guildRole) return
+                rolesStr += `\`${guildRole.name}\` \n`
+            })
+        }
 
         const Embed = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle(`Current List of Managers`)
             .setTimestamp()
             .addFields(
-                { name: "Users", value: usersStr, inline: true },
-                { name: "Roles", value: rolesStr, inline: true }
+                { name: "Users", value: usersStr || "`None`", inline: true },
+                { name: "Roles", value: rolesStr || "`None`", inline: true }
             )
         
         return await message.reply({ embeds: [ Embed ] });
